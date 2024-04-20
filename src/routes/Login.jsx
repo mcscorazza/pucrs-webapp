046d678378a33f779c8b2f9597ff18b5d68783fd
Marcos Corazza <mcs.corazza@gmail.com>
2024-04-20 20:33:54 -0300

    Instalado Tailwind

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='w-full h-svh flex items-center bg-slate-800 justify-center flex-col gap-2' >
+      <div className='text-4xl text-white'>Login</div>
+      <div><Link className='text-slate-400' to="/register">Registrar-se</Link></div>
+      <div><Link className='text-slate-400' to="/app">Aplicação</Link></div>
+    </div>
+
   )
 }
 
--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='w-full h-svh flex items-center bg-slate-800 justify-center flex-col gap-2' >
+      <div className='text-4xl text-white'>Register</div>
+      <div><Link className='text-slate-400' to="/">Login</Link></div>
+    </div>
   )
 }
 
